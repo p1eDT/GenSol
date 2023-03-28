0efa7b75091a57b03c9a9ed97bc33ab6c3c84318
p1eDT <error555@yandex.ru>
2023-03-28 23:02:11 +0500

    folder rename

---
+++ GitHome1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GitHome1 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = Console.ReadLine();
+            Console.WriteLine($"Hello, {user}!");
+        }
+    }
+}
