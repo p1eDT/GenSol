609f2e294cca4a448424e1cf1cadd82bdc52ef2e
p1eDT <error555@yandex.ru>
2023-05-27 02:46:27 +0500

    Add NotificationMessagesTests

--- GitHome12/HoversTests.cs
+++ GitHome12/HoversTests.cs
@@ -13,15 +13,29 @@ namespace GitHome12
 
             List<IWebElement> figureDivs=driver.FindElements(By.ClassName("figure")).ToList();
             Actions action = new Actions(driver);
+            int userId = 1;
 
-            foreach(IWebElement figure in figureDivs)
-            {
+            foreach (IWebElement figure in figureDivs)
+            {                
                 action.MoveToElement(figure).Click().Build().Perform();
-                Assert.That(figure.FindElement(By.ClassName("figcaption")).GetCssValue("display"), Is.EqualTo("block"));
-                Assert.That(figure.FindElement(By.TagName("H5")).Text, Is.EqualTo("name: user1"));
+                CheckFigureDisplayValueIs(figure, "block");
+
+                var figures = figureDivs.Where(figures => !figures.Equals(figure)).ToList();
+                CheckFigureDisplayValueIs(figures[0], "none");
+                CheckFigureDisplayValueIs(figures[1], "none");
+                Assert.That(figure.FindElement(By.TagName("H5")).Text, Is.EqualTo("name: user"+ userId));
+
                 figure.FindElement(By.LinkText("View profile")).Click();
+                Assert.Throws<NoSuchElementException>(() => driver.FindElement(By.XPath("//*[contains(., '404')]")));
+
                 driver.Navigate().Back();
+                userId++;
             }
+        }
+
+        public void CheckFigureDisplayValueIs (IWebElement profile,string value)
+        {
+            Assert.That(profile.FindElement(By.ClassName("figcaption")).GetCssValue("display"), Is.EqualTo(value));
         }
     }
 }
